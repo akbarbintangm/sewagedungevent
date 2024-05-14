618014d71938709425f31f7ce2e940427e84f46a
AjiNgrh <82327626+AjiNgrh@users.noreply.github.com>
2024-05-14 18:05:09 +0700

    feat:navbar

--- resources/views/layouts/user/navbar.blade.php
+++ resources/views/layouts/user/navbar.blade.php
@@ -23,97 +23,32 @@
 				</div>
 			</div>
 			<ul class="navbar-nav navbar-nav-hover align-items-lg-center">
-				<li class="nav-item dropdown">
-					<a class="nav-link" data-toggle="dropdown" href="#" href="#" role="button">
+				<li class="nav-item">
+					<a class="nav-link" data-toggle="tooltip" href="#" role="button">
 						<i class="ni ni-ui-04 d-lg-none"></i>
-						<span class="nav-link-inner--text text-white">Components</span>
-					</a>
-					<div class="dropdown-menu dropdown-menu-xl">
-						<div class="dropdown-menu-inner">
-							<a class="media d-flex align-items-center" href="https://demos.creative-tim.com/argon-design-system/docs/getting-started/overview.html">
-								<div class="icon icon-shape bg-gradient-primary rounded-circle text-white">
-									<i class="ni ni-spaceship"></i>
-								</div>
-								<div class="media-body ml-3">
-									<h6 class="heading text-primary mb-md-1">
-										Getting started
-									</h6>
-									<p class="description d-none d-md-inline-block mb-0">
-										Learn how to use compiling Scss,
-										change brand colors and more.
-									</p>
-								</div>
-							</a>
-							<a class="media d-flex align-items-center" href="https://demos.creative-tim.com/argon-design-system/docs/foundation/colors.html">
-								<div class="icon icon-shape bg-gradient-success rounded-circle text-white">
-									<i class="ni ni-palette"></i>
-								</div>
-								<div class="media-body ml-3">
-									<h6 class="heading text-primary mb-md-1">
-										Foundation
-									</h6>
-									<p class="description d-none d-md-inline-block mb-0">
-										Learn more about colors,
-										typography, icons and the grid
-										system we used for .
-									</p>
-								</div>
-							</a>
-							<a class="media d-flex align-items-center" href="https://demos.creative-tim.com/argon-design-system/docs/components/alerts.html">
-								<div class="icon icon-shape bg-gradient-warning rounded-circle text-white">
-									<i class="ni ni-ui-04"></i>
-								</div>
-								<div class="media-body ml-3">
-									<h5 class="heading text-warning mb-md-1">
-										Components
-									</h5>
-									<p class="description d-none d-md-inline-block mb-0">
-										Browse our 50 beautiful
-										handcrafted components offered
-										in the Free version.
-									</p>
-								</div>
-							</a>
-						</div>
-					</div>
-				</li>
-				<li class="nav-item dropdown">
-					<a class="nav-link" data-toggle="dropdown" href="#" href="#" role="button">
-						<i class="ni ni-collection d-lg-none"></i>
-						<span class="nav-link-inner--text text-white">Examples</span>
-					</a>
-					<div class="dropdown-menu">
-						<a class="dropdown-item" href="../examples/landing.html">Landing</a>
-						<a class="dropdown-item" href="../examples/profile.html">Profile</a>
-						<a class="dropdown-item" href="../examples/login.html">Login</a>
-						<a class="dropdown-item" href="../examples/register.html">Register</a>
-					</div>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link nav-link-icon" data-toggle="tooltip" href="https://www.facebook.com/CreativeTim/" target="_blank" title="Like us on Facebook">
-						<i class="fa fa-facebook-square text-white"></i>
-						<span class="nav-link-inner--text d-lg-none">Facebook</span>
-					</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link nav-link-icon" data-toggle="tooltip" href="https://www.instagram.com/creativetimofficial" target="_blank" title="Follow us on Instagram">
-						<i class="fa fa-instagram text-white"></i>
-						<span class="nav-link-inner--text d-lg-none">Instagram</span>
-					</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link nav-link-icon" data-toggle="tooltip" href="https://twitter.com/creativetim" target="_blank" title="Follow us on Twitter">
-						<i class="fa fa-twitter-square text-white"></i>
-						<span class="nav-link-inner--text d-lg-none">Twitter</span>
-					</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link nav-link-icon" data-toggle="tooltip" href="https://github.com/creativetimofficial/argon-design-system" target="_blank" title="Star us on Github">
-						<i class="fa fa-github text-white"></i>
-						<span class="nav-link-inner--text d-lg-none">Github</span>
+						<span class="nav-link-inner--text text-white">Beranda</span>
 					</a>
 				</li>
 			</ul>
+            <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+                <li class="nav-item">
+					<a class="nav-link nav-link-icon" data-toggle="tooltip" href="#" title="">
+						<i class="fa fa-list-alt fa-lg text-white"></i>
+						<span class="nav-link-inner--text d-lg-none">Riwayat Transaksi</span>
+					</a>
+				</li>
+                <li class="nav-item dropdown">
+					<a class="nav-link nav-link-icon" data-toggle="dropdown" href="#" title="User">
+						<i class="fa fa-user fa-lg text-white"></i>
+						<span class="nav-link-inner--text d-lg-none">User</span>
+					</a>
+                    <div class="dropdown-menu">
+						<a class="dropdown-item" href="../examples/profile.html">Profile</a>
+						<a class="dropdown-item text-danger" href="../examples/login.html">Logout</a>
+						<a class="dropdown-item" href="../examples/register.html">Register</a>
+					</div>
+				</li>
+            </ul>
 		</div>
 	</div>
 </nav>
