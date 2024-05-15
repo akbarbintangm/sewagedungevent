038f5168084be1309561b30dbd2828cf17cadc1f
Akbar Bintang M <akbarbintangmahendra@gmail.com>
2024-05-15 20:30:18 +0700

    Fix: button detail rooms

--- resources/views/components/admin/building/unverified/action.blade.php
+++ resources/views/components/admin/building/unverified/action.blade.php
@@ -1,4 +1,10 @@
-<a class="btn btn-outline-primary btn-sm mb-0" href="{{ route('detailPageBuilding:admin', ['id' => $id]) }}" type="button">Details</a>
+@if (Auth::user()->type_user === 'ADMINISTRATOR')
+	<a class="btn btn-outline-primary btn-sm mb-0" href="{{ route('detailPageBuilding:admin', ['id' => $id]) }}" type="button">Details</a>
+@elseif (Auth::user()->type_user === 'PEMILIK_GEDUNG')
+	<a class="btn btn-outline-primary btn-sm mb-0" href="{{ route('detailPageBuilding:owner', ['id' => $id]) }}" type="button">Details</a>
+@elseif (Auth::user()->type_user === 'ADMIN_ENTRY')
+	<a class="btn btn-outline-primary btn-sm mb-0" href="{{ route('detailPageBuilding:admin-entry', ['id' => $id]) }}" type="button">Details</a>
+@endif
 @if (Auth::user()->type_user !== 'PEMILIK_GEDUNG')
 	<button class="btn btn-outline-success btn-sm mb-0" data-id="{{ $id }}" onclick="verifyRoom({{ $id }})" type="button">Verifikasi</button>
 @endif
--- resources/views/components/admin/building/verified/action.blade.php
+++ resources/views/components/admin/building/verified/action.blade.php
@@ -1,2 +1,8 @@
-<a class="btn btn-outline-primary btn-sm mb-0" href="{{ route('detailPageBuilding:admin', ['id' => $id]) }}" type="button">Details</a>
+@if (Auth::user()->type_user === 'ADMINISTRATOR')
+	<a class="btn btn-outline-primary btn-sm mb-0" href="{{ route('detailPageBuilding:admin', ['id' => $id]) }}" type="button">Details</a>
+@elseif (Auth::user()->type_user === 'PEMILIK_GEDUNG')
+	<a class="btn btn-outline-primary btn-sm mb-0" href="{{ route('detailPageBuilding:owner', ['id' => $id]) }}" type="button">Details</a>
+@elseif (Auth::user()->type_user === 'ADMIN_ENTRY')
+	<a class="btn btn-outline-primary btn-sm mb-0" href="{{ route('detailPageBuilding:admin-entry', ['id' => $id]) }}" type="button">Details</a>
+@endif
 <button class="btn btn-outline-danger btn-sm mb-0" data-id="{{ $id }}" onclick="deleteRoom({{ $id }})" type="button">Hapus</button>
